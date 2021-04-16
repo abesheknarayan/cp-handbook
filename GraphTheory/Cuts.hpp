c66905ed768409e968646d1c7bcd7e32c75a135c
abesheknarayan <abeshek.ramanathan2000@gmail.com>
2021-04-16 15:04:31 +0530

    [Add]: NT and bridge

---
+++ GraphTheory/Cuts.hpp
@@ -0,0 +1,52 @@
+struct Cuts
+{
+  vector<vll> v;
+  vll vis;
+  vll tin, tout, low;
+
+  Cuts(ll n)
+  {
+    v = vector<vll>(n, vll());
+    vis = vll(n, 0);
+    tin = vll(n, 0);
+    tout = vll(n, 0);
+    low = vll(n,INF);
+  }
+
+  // returns all bridges of the graph in edge form
+  vector<pll> bridges()
+  {
+    ll num = 0;
+    vector<pll> bridge;
+    function<void(ll,ll)> dfs = [&](ll x,ll p) {
+      vis[x] = 1;
+      tin[x] = num;
+      low[x] = num;
+      num++;
+      for (auto it : v[x])
+      {
+        if(it == p)continue;
+        if (vis[it]) // back edge
+        {
+          low[x] = min(tin[it],low[x]);
+        }
+        else
+        {
+          dfs(it,x);
+          low[x] = min(low[it],low[x]);
+          if(low[it] > tin[x])
+          {
+            bridge.push_back({x,it});
+          }
+        }
+      }
+      tout[x] = num;
+    };
+    dfs(0,-1);
+    return bridge;
+  }
+  // returns all cut vertices
+  vll cutVertex()
+  {
+  }
+};
---
+++ Math/NumberTheory.hpp
@@ -0,0 +1,55 @@
+struct NT
+{
+
+    // returns phi of given number in O(sqrt(n))
+    // multiplicative
+    ll calc_phi(ll x)
+    {
+        ll res = x;
+        for (ll i = 2; i * i <= x; i++)
+        {
+            if (x % i == 0)
+            {
+                while (x % i == 0)
+                    x /= i;
+
+                res -= res / i;
+            }
+        }
+        if (x > 1)
+        {
+            res -= res / x;
+        }
+        return res;
+    }
+
+    // calculates phi from 1 to n in O(nlogn)
+    // using guass formula sum(phi(x)) where x|n  = n
+    vector<ll> calc_phi_1_to_n(ll n)
+    {
+        vector<ll> phi(n + 1);
+        phi[0] = 0;
+        phi[1] = 1;
+        for (ll i = 2; i <= n; i++)
+        {
+            phi[i] = i - 1;
+        }
+        for (ll i = 2; i <= n; i++)
+        {
+            for (ll j = 2 * i; j <= n; j+=i)
+            {
+                phi[j] -= phi[i];
+            }
+        }
+        return phi;
+    }
+
+    // returns primes till n in O(n)
+    vector<ll> liear_sieve(ll n)
+    {
+        vll primes(n,1);
+        
+    }
+
+    // TODO : Discrete log (Baby step giant step) , primitive roots
+};
