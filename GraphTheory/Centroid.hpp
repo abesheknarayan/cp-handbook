8bbc5202f48ed560ec9fa98f8a1d946511f2e62c
abesheknarayan <abeshek.ramanathan2000@gmail.com>
2021-05-22 08:07:44 +0530

    [Add]: Add level centroids

--- GraphTheory/Centroid.hpp
+++ GraphTheory/Centroid.hpp
@@ -1,47 +1,81 @@
-struct CentroidDecomp{
-  ll n;
+struct CentroidDecomp
+{
+  ll n, k1, k2;
   vector<vll> v;
+  vll marked; // for marking already visited centroids
   vll subtreeSize;
-
-  CentroidDecomp(ll _n)
+  long long ans;
+  o_tree tr;
+  // segTree sg;
+ 
+  CentroidDecomp(ll _n, ll _k1, ll _k2)
   {
     n = _n;
-    v = vector<vll>(n,vll());
-    subtreeSize.assign(n,1);
+    k1 = _k1;
+    k2 = _k2;
+    ans = 0;
+    v = vector<vll>(n, vll());
+    subtreeSize.assign(n, 1);
+    marked.assign(n, 0);
+    // sg = segTree(2e5+5);
   }
-
-  void addEdge(ll x,ll y)
+ 
+  void addEdge(ll x, ll y)
   {
     v[x].push_back(y);
     v[y].push_back(x);
   }
-
-  ll findCentroid()
+ 
+  ll getCentroid(ll node)
   {
-    ll centroid = -1;
-    
-    // returns size of subtree
-    function<void(ll,ll)> dfs1 = [&](ll x,ll p)
+    function<void(ll, ll)> get_subsize = [&](ll x, ll p)
     {
-      bool ok = true;
-      for(auto it:v[x])
+      subtreeSize[x] = 1;
+      for (auto it : v[x])
       {
-        if(it == p)continue;
-        dfs1(it,x);
-        subtreeSize[x]+=subtreeSize[it];
-        if(subtreeSize[it] > n/2)
-        {
-          ok = false;
-        }
-      }
-      if(subtreeSize[x] < n/2) ok = false;
-      if(ok)
-      {
-        centroid = x;
+        if (it == p || marked[it])
+          continue;
+        get_subsize(it, x);
+        subtreeSize[x] += subtreeSize[it];
       }
     };
-    dfs1(0,-1);
-    assert(centroid!=-1);
-    return centroid;
+    get_subsize(node, -1);
+    function<ll(ll, ll)> dfs = [&](ll x, ll p)
+    {
+      for (auto it : v[x])
+      {
+        if (it == p || marked[it])
+          continue;
+        if (subtreeSize[it] > subtreeSize[node] / 2)
+        {
+          return dfs(it, x);
+        }
+      }
+      return x;
+    };
+    return dfs(node, -1);
+  }
+ 
+  void decompose_graph()
+  {
+    function<void(ll)> decompose = [&](ll x)
+    {
+      ll centroid_now = getCentroid(x);
+      marked[centroid_now] = 1;
+ 
+      ll maxdep = -1;
+      
+      // do computations here
+
+      for (auto next : v[centroid_now])
+      {
+        if (!marked[next])
+        {
+          decompose(next);
+        }
+      }
+      marked[centroid_now] = 0;
+    };
+    decompose(0);
   }
 };
